97b15c17ecea1283f205ff4aec59867480984739
Pablo Gonzálvez del Pino <pablogonzalvezdelpino99@gmail.com>
2022-10-26 20:33:21 +0200

    Tablas

---
+++ SwiftUI_TablasApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TablasApp.swift
+//  SwiftUI_Tablas
+//
+//  Created by Apps2T on 25/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TablasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NinjaListView()
+        }
+    }
+}
